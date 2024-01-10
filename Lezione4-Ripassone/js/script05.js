54d02baffe9b7cc9c37988a8ca2b0e67d049d905
simoneImmLavoro <simone.astore@edu.immaginazioneelavoro.it>
2024-01-10 22:15:24 +0100

    mess

--- Lezione4-Ripassone/js/script05.js
+++ Lezione4-Ripassone/js/script05.js
@@ -7,19 +7,23 @@ let counterNull= 0;
 let counterLow= 0;
 let counterHight= 0;
 
+
 for(let i = 0; i < 400; i++){
     let randomNum = Math.ceil(Math.random() * 400);
-    if((randomNum >= 100) && (randomNum <= 400)){
-        numList.innerHTML += `<li> ${randomNum} - Il tuo numero è tra 100 e 400 </li>`;
-        counterHight++;
-    }else if ((randomNum >= 20) && (randomNum < 100)) {
+    if ((randomNum >= 20) && (randomNum < 100)) {
         numList.innerHTML += `<li> ${randomNum} - Il tuo numero è tra 20 e 100 </li>`;
         counterLow++;
+    } else if((randomNum >= 100) && (randomNum <= 400)){
+        numList.innerHTML += `<li> ${randomNum} - Il tuo numero è tra 100 e 400 </li>`;
+        counterHight++;
     } else {
         numList.innerHTML += `<li> ${randomNum} - Il tuo numero è fuori range</li>`;
         counterNull++;
     }
 }
+
+
+
 
 counters.innerHTML += `<h4>I numeri fuori range sono ${counterNull}</h4>`;
 counters.innerHTML += `<h4>I numeri tra 20 e 100 sono ${counterLow}</h4>`;
