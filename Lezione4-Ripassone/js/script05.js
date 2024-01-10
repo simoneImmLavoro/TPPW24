c22bed57ed10dd12e8841078eb37026f9bc6271e
simoneImmLavoro <simone.astore@edu.immaginazioneelavoro.it>
2024-01-10 11:09:02 +0100

    Updated

--- Lezione4-Ripassone/js/script05.js
+++ Lezione4-Ripassone/js/script05.js
@@ -1,8 +1,16 @@
 //Crea un elenco di 400 numeri random. (ATT: puoi anche svolgere soolo la parte del controllo del numero)Per ogni numero dire se questo si trova tra 20 e 100 oppure tra 20 e 400.
 
-let numero = 75; //questo numero si trova tra 20 e 100
+let numList = document.getElementById("numList");
 
-let numero2 = 112; //questo numero si trova tra 20 e 400
+numList.innerHTML = `<li> ${randomNum} - Il tuo numero è tra 100 e 400 </li>`
 
-
-
+for(let i = 0; i < 400; i++){
+    let randomNum = Math.ceil(Math.random() * 400);
+    if((randomNum >= 100) && (randomNum <= 400)){
+        numList.innerHTML = `<li> ${randomNum} - Il tuo numero è tra 100 e 400 </li>`;
+    }else if ((randomNum >= 20) && (randomNum < 100)) {
+        numList.innerHTML = `<li> ${randomNum} - Il tuo numero è tra 20 e 100 </li>`;
+    } else {
+        numList.innerHTML = `<li> ${randomNum} - Il tuo numero è fuori range</li>`;
+    }
+}
